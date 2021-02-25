d7173a4c12b22895d142431cc53b742d583ee834
snchz29 <sasha4888.terehov@gmail.com>
2021-02-25 15:21:03 +0700

    first step

---
+++ Lb_3/gui/MainWindow.py
@@ -0,0 +1,9 @@
+from PyQt5.QtGui import QIntValidator
+from PyQt5 import QtCore, QtWidgets
+
+
+class MainWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Nechepurenko & Terekhov Limited")
+
