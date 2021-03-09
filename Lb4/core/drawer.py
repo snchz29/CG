1dbf5452098dbc398b8226183b2733af685d05b4
snchz29 <sasha4888.terehov@gmail.com>
2021-03-09 22:54:12 +0300

    add recursive implementation

--- Lb4/core/drawer.py
+++ Lb4/core/drawer.py
@@ -25,12 +25,69 @@ class IFSFractalGenerator:
         return fn_index, self._functions[fn_index](previous_x, previous_y)
 
 
-class Drawer(metaclass=SingletonMeta):
+class RecFractalGenerator:
+    rot = [
+        [math.cos, lambda theta: -math.sin(theta)],
+        [math.sin, math.cos],
+    ]
 
+    def __init__(self):
+        self._start_shape = [
+            [-1, 1],
+            [1, 1],
+            [1, -1],
+            [-1, -1],
+        ]
+        self._sqs = []
+        self._max_depth = 5
+        self._params = [
+            [0, 1, 1, 0, -.4],
+            [0, 1, 1, 0, .4],
+            [- 60 * math.pi / 180, 1, 1, -.5, -.2],
+            [60 * math.pi / 180, 1, 1, .5, -.2],
+            [0, .1, .5, 0, -2.4],
+        ]
+
+    def get(self, depth):
+        self._max_depth = depth
+        self._sqs = []
+        self.rec(self._start_shape, *self._params[0])
+        self.rec(self._start_shape, *self._params[1])
+        self.rec(self._start_shape, *self._params[2])
+        self.rec(self._start_shape, *self._params[3])
+        self.rec(self._start_shape, *self._params[4])
+        return list(filter(lambda x: x is not None, self._sqs))
+
+    def rec(self, shape: list, theta: float = 0, dx: float = 1, dy: float = 1, cx: float = 0, cy: float = 0,
+            depth=0):
+        new_shape = []
+        if depth > self._max_depth:
+            return
+        for s in shape:
+            new_shape.append(
+                [(self.rot[0][0](theta) * s[0] + cx) * dx * .5 + (self.rot[0][1](theta) * s[1] + cx) * dy * .5,
+                 (self.rot[1][0](theta) * s[0] + cy) * dx * .5 + (self.rot[1][1](theta) * s[1] + cy) * dy * .5])
+        if depth == self._max_depth - 1:
+            self._sqs.append(self.rec(new_shape, *self._params[0], depth + 1))
+            self._sqs.append(self.rec(new_shape, *self._params[1], depth + 1))
+            self._sqs.append(self.rec(new_shape, *self._params[2], depth + 1))
+            self._sqs.append(self.rec(new_shape, *self._params[3], depth + 1))
+            self._sqs.append(self.rec(new_shape, *self._params[4], depth + 1))
+        else:
+            self.rec(new_shape, *self._params[0], depth + 1)
+            self.rec(new_shape, *self._params[1], depth + 1)
+            self.rec(new_shape, *self._params[2], depth + 1)
+            self.rec(new_shape, *self._params[3], depth + 1)
+            self.rec(new_shape, *self._params[4], depth + 1)
+        return new_shape
+
+
+class Drawer(metaclass=SingletonMeta):
     def __init__(self):
         self._x = 0
         self._y = 0
         self._fractal_generator = IFSFractalGenerator(leaf_params)
+        self._rec_fractal_generator = RecFractalGenerator()
 
     def paint(self, num_iterations=100000, color_mode=False):
         current_x, current_y = self._x, self._y
@@ -41,8 +98,18 @@ class Drawer(metaclass=SingletonMeta):
             if color_mode:
                 glColor3f(*color_params[color_index])
             glVertex2f(current_x * 1 / 2, current_y * 1 / 2)
-
         glEnd()
+
+    def paint_recursive(self, depth):
+        sqs = self._rec_fractal_generator.get(depth)
+        for s in sqs:
+            glColor3f(1, 0, 0)
+            glBegin(GL_LINE_LOOP)
+            glVertex2f(s[0][0] * .8, s[0][1] * .8)
+            glVertex2f(s[1][0] * .8, s[1][1] * .8)
+            glVertex2f(s[2][0] * .8, s[2][1] * .8)
+            glVertex2f(s[3][0] * .8, s[3][1] * .8)
+            glEnd()
 
 
 colors = [
--- Lb4/gui/control_panel.py
+++ Lb4/gui/control_panel.py
@@ -1,4 +1,5 @@
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIntValidator
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QLineEdit, QCheckBox
 
 from Lb4.core.drawer import Drawer
@@ -8,6 +9,8 @@ class ControlPanel(QWidget):
     def __init__(self):
         super().__init__()
         self._drawer = Drawer()
+        self._iterations_validator = QIntValidator(1, 100_000)
+        self._depth_validator = QIntValidator(1, 6)
         self._draw_area = None
         self._main_layout = QVBoxLayout()
         self.setLayout(self._main_layout)
@@ -21,31 +24,40 @@ class ControlPanel(QWidget):
         self._iterations_number_label = QLabel("Число точек:")
         self._iterations_number_input = QLineEdit("10000")
         self._color_checkbox = QCheckBox("Использовать цветовую схему")
+        self._recursive_checkbox = QCheckBox("Использовать рекурсивную реализацию")
+        self._depth_label = QLabel("Глубина рекурсии:")
+        self._depth_input = QLineEdit("5")
+        self._iterations_number_input.setValidator(self._iterations_validator)
+        self._depth_input.setValidator(self._depth_validator)
         self._iterations_number_input.textChanged.connect(self.on_change)
+        self._depth_input.textChanged.connect(self.on_change)
         self._color_checkbox.stateChanged.connect(self.on_change)
+        self._recursive_checkbox.stateChanged.connect(self.on_change)
 
     def __layout_components(self):
         self._main_layout.addWidget(self._header_label)
+        self._main_layout.addWidget(self._recursive_checkbox)
         self._main_layout.addWidget(self._iterations_number_label)
         self._main_layout.addWidget(self._iterations_number_input)
         self._main_layout.addWidget(self._color_checkbox)
+        self._main_layout.addWidget(self._depth_label)
+        self._main_layout.addWidget(self._depth_input)
 
     def paint(self):
-        iterations_number = self.__try_get_iteration_number()
-        is_color_mode = self._color_checkbox.isChecked()
-        self._drawer.paint(iterations_number, is_color_mode)
+        rec_flag = self._recursive_checkbox.isChecked()
+        self._iterations_number_input.setDisabled(rec_flag)
+        self._color_checkbox.setDisabled(rec_flag)
+        self._depth_input.setEnabled(rec_flag)
+        if rec_flag:
+            depth = self._depth_input.text()
+            self._drawer.paint_recursive(int(depth if depth != '' else 1))
+        else:
+            iterations_number = self._iterations_number_input.text()
+            is_color_mode = self._color_checkbox.isChecked()
+            self._drawer.paint(int(iterations_number if iterations_number != '' else 10), is_color_mode)
 
     def on_change(self):
         self._draw_area.update()
 
     def set_draw_area(self, draw_area):
         self._draw_area = draw_area
-
-    def __try_get_iteration_number(self):
-        try:
-            number = int(self._iterations_number_input.text())
-        except:
-            pass
-        else:
-            return number
-        return 10000
