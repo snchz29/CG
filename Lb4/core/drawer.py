3b8078c5e242e8ad7711b852a2602b05d7f512ae
snchz29 <sasha4888.terehov@gmail.com>
2021-03-09 21:40:43 +0300

    recursive implementation

--- Lb4/core/drawer.py
+++ Lb4/core/drawer.py
@@ -1,50 +1,87 @@
-import logging
-import random
 import math
+import random
 
 from OpenGL.GL import *
-
-
-class IFSFractalGenerator:
-    def __init__(self, params_list):
-        self._function_generator = lambda a, b, c, d, e, f: lambda x0, y0: (a * x0 + b * y0 + e, c * x0 + d * y0 + f)
-        self._functions = [self._function_generator(*params) for params in params_list]
-
-    def getNext(self, xx, yy):
-        return random.choice(self._functions)(xx, yy)
 
 
 class Drawer:
     def __init__(self):
         self._x = 0
         self._y = 0
-        self._fractal_generator = IFSFractalGenerator(leaf_params)
+        self._rec_fractal_generator = RecFractalGenerator()
 
     def paint(self):
-        num_iterations = 100000
-        t_X, t_Y = self._x, self._y
-        glColor3f(1., 0., 0.)
-        glBegin(GL_POINTS)
-        for i in range(num_iterations):
-            try:
-                t_X, t_Y = self._fractal_generator.getNext(t_X, t_Y)
-            except Exception as e:
-                logging.error(e)
-            # logging.info(f"{t_X}, {t_Y}")
-            glVertex2f(t_X * 1 / 2, t_Y * 1 / 2)
+        sqs = self._rec_fractal_generator.get()
+        colors = [
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1],
+            [1, 1, 0],
+            [0, 1, 1],
+            [0, 0, 0],
+        ]
+        idx = 0
+        for s in sqs:
+            glColor3f(1,0,0)
+            idx = (idx + 1) % len(colors)
+            glBegin(GL_LINE_LOOP)
+            glVertex2f(s[0][0] * 1, s[0][1] * 1)
+            glVertex2f(s[1][0] * 1, s[1][1] * 1)
+            glVertex2f(s[2][0] * 1, s[2][1] * 1)
+            glVertex2f(s[3][0] * 1, s[3][1] * 1)
+            glEnd()
 
-        glEnd()
 
+class RecFractalGenerator:
+    rot = [
+        [math.cos, lambda theta: -math.sin(theta)],
+        [math.sin, math.cos],
+    ]
 
-leaf_params = [
-    [1., 0., 0., 1., -.02, .69],
-    [1 / 2, -math.sqrt(3) / 2, math.sqrt(3) / 2, 1 / 2, .8, -.12],
-    [0.423, 0.906, -0.906, 0.423, -.75, -.19],
-    [1, 0, 0, 1, .01, -.28],
-    [.13, 0, 0, .43, .03, -.73]
-]
+    def __init__(self):
+        self._start_shape = [
+            [-1, 1],
+            [1, 1],
+            [1, -1],
+            [-1, -1],
+        ]
+        self._sqs = []
+        self._max_depth = 5
+        self._params = [
+            [0, 1, 1, 0, -.4],
+            [0, 1, 1, 0, .4],
+            [- 60, 1, 1, -.5, -.2],
+            [60, 1, 1, .5, -.2],
+            [0, .1, .5, 0, -2.4],
+        ]
 
-for index in range(len(leaf_params)):
-    for inx2 in range(len(leaf_params[index])):
-        if inx2 < 4:
-            leaf_params[index][inx2] *= 1 / 2
+    def get(self):
+        self.rec(self._start_shape, *self._params[0])
+        self.rec(self._start_shape, *self._params[1])
+        self.rec(self._start_shape, *self._params[2])
+        self.rec(self._start_shape, *self._params[3])
+        self.rec(self._start_shape, *self._params[4])
+        self._sqs = list(filter(lambda x: x is not None, self._sqs))
+        return self._sqs
+
+    def rec(self, shape: list, theta: float = 0, dx: float = 1, dy: float = 1, cx: float = 0, cy: float = 0, depth=0):
+        new_shape = []
+        if depth > self._max_depth:
+            return
+        for s in shape:
+            new_shape.append(
+                [(self.rot[0][0](theta) * s[0] + cx) * dx * .5 + (self.rot[0][1](theta) * s[1] + cx) * dy * .5,
+                 (self.rot[1][0](theta) * s[0] + cy) * dx * .5 + (self.rot[1][1](theta) * s[1] + cy) * dy * .5])
+        if depth == self._max_depth - 1:
+            self._sqs.append(self.rec(new_shape, *self._params[0], depth + 1))
+            self._sqs.append(self.rec(new_shape, *self._params[1], depth + 1))
+            self._sqs.append(self.rec(new_shape, *self._params[2], depth + 1))
+            self._sqs.append(self.rec(new_shape, *self._params[3], depth + 1))
+            self._sqs.append(self.rec(new_shape, *self._params[4], depth + 1))
+        else:
+            self.rec(new_shape, *self._params[0], depth + 1)
+            self.rec(new_shape, *self._params[1], depth + 1)
+            self.rec(new_shape, *self._params[2], depth + 1)
+            self.rec(new_shape, *self._params[3], depth + 1)
+            self.rec(new_shape, *self._params[4], depth + 1)
+        return new_shape
